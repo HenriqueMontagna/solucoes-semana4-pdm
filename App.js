6ad2a1af17a5e6ed9b13825218be0083f7fc7ac2
Henrique La Montagna <henriquelamontagna@gmail.com>
2020-09-13 12:36:20 -0300

    Ex aula 11-09-2020

--- App.js
+++ App.js
@@ -1,12 +1,60 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, FlatList, Keyboard, StyleSheet, Text, TextInput, View } from 'react-native';
+import PrevisaoItem from './components/PrevisaoItem';
 
 export default function App() {
+
+  const [cidade, setCidade] = useState('')
+
+  const[previsoes, setPrevisoes] = useState('')
+
+  const capturarCidade = (cidade) => {
+    setCidade(cidade)
+  }
+
+  const endPoint = "https://api.openweathermap.org/data/2.5/forecast?lang=pt_br&units=metric&q="
+
+  const apiKey = "106ef2f5efca972629c768cfd89a6816"
+
+
+  const obterPrevisoes = () => {
+    setPrevisoes([]);
+    const target = endPoint + cidade + "&appid=" + apiKey;
+    fetch(target)
+    .then( (dados) => dados.json())
+    .then( (dados) => {
+      setPrevisoes(dados["list"])
+      Keyboard.dismiss()
+    });
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.entrada}>
+        <TextInput 
+          style = {styles.nomeCidade}
+          placeholder = "Digite o Nome da Cidade"
+          value = {cidade}
+          onChangeText = {capturarCidade}
+        />
+        <Button 
+          title = "ok"
+          onPress = {obterPrevisoes}
+        />
+      </View>
+
+      <FlatList 
+        data={previsoes}
+        renderItem={
+          previsao => (
+            <PrevisaoItem previsao = {previsao.item} />
+          )
+
+        }
+      />
+    
     </View>
   );
 }
@@ -14,8 +62,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40,
+    flexDirection: 'column',
+    backgroundColor: '#fff'
   },
+
+  nomeCidade: {
+    padding: 12,
+    borderBottomColor: '#BB96F3',
+    borderBottomWidth: 2,
+    textAlign: "left",
+    flexGrow: 0.9
+  },
+
+  entrada: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginBottom: 10
+  }
+
+
 });
---
+++ components/PrevisaoItem.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import {
+    View,
+    Text,
+    StyleSheet, Image
+} from 'react-native';
+import Cartao from './Cartao';
+
+const PrevisaoItem = (props) => {
+    return (
+        <Cartao estilos={styles.cartao}>
+            <View style={styles.tela}>
+                <Image
+                    style={styles.imagem}
+                    source={
+                        {
+                            uri: "https://openweathermap.org/img/wn/" + props.previsao.weather[0].icon + ".png"
+                        }
+                    }
+                />
+                <View>
+                    <View style={styles.primeiraLinha}>
+                        <Text>{new Date(props.previsao.dt * 1000).toLocaleTimeString()}
+                        - {props.previsao.weather[0].description}</Text>
+                    </View>
+                    <View style={styles.segundaLinha}>
+                        <Text style={styles.valor}>Min:{props.previsao.main.temp_min + "\u00B0"}</Text>
+                        <Text style={styles.valor}>Min:{props.previsao.main.temp_max + "\u00B0"}</Text>
+                        <Text style={styles.valor}>Min:{props.previsao.main.humidity}%</Text>
+                    </View>
+                </View>
+            </View>
+        </Cartao>
+    )
+}
+
+const styles = StyleSheet.create({
+
+    cartao: {
+        marginBottom: 8
+    },
+
+    tela: {
+        flexDirection: 'row'
+    },
+
+    imagem: {
+        width: 50,
+
+    },
+
+    primeiraLinha: {
+        justifyContent: 'center',
+        flexDirection: 'row'
+    },
+
+    segundaLinha: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        marginTop: 4,
+        borderTopWidth: 1,
+        borderTopColor: "#DDD"
+    }
+
+})
+
+export default PrevisaoItem;
